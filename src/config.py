983362261e0a50328a264b2e5d3ad66cbb6cdf4f
Matheus Mauricio <lhr3@hotmail.com>
2024-04-06 17:50:08 -0300

    adicoes no readme

--- src/config.py
+++ src/config.py
@@ -1,5 +1,4 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import os
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="server_side_", env_file=".env", env_file_encoding="utf-8")
